24e06aecff6404d1b82afc4d0b22ee4016a63b6d
eusebio suarez <eusebiosuaresmartines@gmail.com>
2024-06-13 07:35:26 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,22 @@
+let bookTitle: string = "TypeScript Basics";
+const libraryName: string = "My Library";
+let numberOfBooks: number = 18; 
+let isLibraryOpen: boolean = true;
+
+let titulo: string = ""
+let autor: string = ""
+let genero: string = ""
+let idioma: string = ""
+let precio: number = 10
+let formato: string = ""
+let isbn : string = ""
+let decripcio : string = ""
+let estado : string = ""
+let ubicacion : string = ""
+let fecha_publicacion : number = 2024
+let editorial : string = ""
+let paginas : number = 100
+let dimenciones : string = ""
+let peso : string = ""
+let disponible : boolean = true
+
